15d19cb64a6e14e56d8a1d2ec2dea0173b3c067d
Vaishu201012 <priyavaishu2010@gmail.com>
2026-09-10 15:23:13 +0530

    Fix production API and CORS

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://it-service-desk1-2.onrender.com'
+  baseURL: 'https://it-service-desk1-2.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
